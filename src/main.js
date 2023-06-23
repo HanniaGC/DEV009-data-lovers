00a878ac6b1418aa4f882f5271891f6937d766b6
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-06-22 20:39:55 -0500

    Muestra la info de personajes HU1

--- src/main.js
+++ src/main.js
@@ -2,10 +2,24 @@ import data from "./data/rickandmorty/rickandmorty.js";
 //console.log(data);
 const charactersImg = document.querySelector(".characters__img");
 
-data.results.forEach((element) => {
+data.results.slice(0, 29).forEach((element) => {
   charactersImg.innerHTML += `
   <div class="img">
-    <div class="img--label">${element.name}</div>
-    <img src="${element.image}" />
+      <img src="${element.image}" />
+      <div class="img--label">${element.name}</div>
   </div>`;
-});
+})
+
+const charactersDate = document.querySelector(".characters__date")
+
+data.results.slice(0, 29).forEach((descrip) => {
+  charactersDate.innerHTML += `
+  <div class="imgen">
+      <img src="${descrip.image}" />
+      <p class="nam">${descrip.name}</p>
+      <p class="species">${descrip.species}</p>
+      <p class="gender">Origen: ${descrip.gender}</p>
+      <p class="origin">${descrip.origin.name}</p>
+      <p class="live">${descrip.location.name}</p>
+  </div>`;
+})
