d7e30019f0002c0a831180d6d83b6723a4b46d8c
HanniaGC <hannia_gc@outlook.com>
2023-06-26 08:39:19 -0600

    intento de interactuar con la data

--- src/data.js
+++ src/data.js
@@ -4,6 +4,31 @@ export const example = () => {
   return 'example';
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//document.getElementById("info_character").style.display = "none";
+
+//const select = document.getElementById("characters_img");
+//const dateCharacter = document.getElementById("info_character");
+
+//select.addEventListener("click", function (){
+//if (dateCharacter.style.display === "none") {
+  //dateCharacter.style.display = "block";
+  //document.getElementById("characters").style.display = "none";
+//} else {
+  //dateCharacter.style.display = "none";
+//}
+//})
+
+const charactersImg = document.getElementByClass(".characters_img");
+const dateCharacter = document.getElementByClass(".imgen");
+
+charactersImg.addEventListener('click', function (event) {
+  const clickedImg = event.target.closest(".img");
+  if (clickedImg ) {
+    dateCharacter.style.display = "block";
+    document.getElementByClass(".characters_img").style.display = "none";
+  } 
+});
+
+//dateCharacter.addEventListener('click', function () {
+  //dateCharacter.style.display = "none";
+//});
--- src/main.js
+++ src/main.js
@@ -1,6 +1,41 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { example } from './data.js';
 
-console.log(example, data);
+import data from './data/rickandmorty/rickandmorty.js';
+
+console.log(data.results);
+
+const charactersImg = document.querySelector(".characters_img");
+
+data.results.slice(0, 30).forEach(element => {
+    charactersImg.innerHTML += `
+        <div class="img">
+        <img src="${element.image}" />
+            <div class="img--label">${element.name}</div>
+        
+        </div>`;
+        
+});
+
+const charactersDate = document.querySelector("#info_character")
+
+data.results.slice(0, 29).forEach((descrip) => {
+  charactersDate.innerHTML += `
+  <div class="imgen">
+      <img src="${descrip.image}" />
+      <p class="nam">Nombre: ${descrip.name}</p>
+      <p class="species">Especie: ${descrip.species}</p>
+      <p class="gender">Genero: ${descrip.gender}</p>
+      <p class="origin">Lugar de origen: ${descrip.origin.name}</p>
+      <p class="live">Lugar donde vive: ${descrip.location.name}</p>
+  </div>`;
+})
+
+//const showData = document.querySelector(".imge_character");
+
+//data.results.forEach(Characterinf => {
+//    info_character.innerHTML += `
+   // < div class="imge_character">
+   // <img src="${element.image}" />
+   // <div class = 
+
+   //</div> )
