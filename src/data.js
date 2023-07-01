cd2df6495553f978c87e79b406795d1e1f036b5a
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-06-30 19:45:45 -0500

    Filtrado de personajes por especie

--- src/data.js
+++ src/data.js
@@ -1,7 +1,20 @@
 // estas funciones son de ejemplo
-export const data = () => {
-  return "data";
-};
+//export const filter = () => {
+////return "characterData";
+//};
+//console.log(characterData)
 
-var newArray = arr.filter(callback(currentValue["url", index["episode", "results"]])
-console.log(newArray)
+//var newArray = arr.filter(callback(currentValue["url", index["episode", "results"]])
+//console.log(newArray)
+
+/*let newArray = CharacterData.filter(item => {
+  return item.name.includes("episode")
+
+}*/
+
+export function filterBySpecies(dataCard, species) {
+  const filteredData = dataCard.filter((element) => {
+    return element.species.toLowerCase() === species.toLowerCase();
+  });
+  return filteredData;
+}
--- src/main.js
+++ src/main.js
@@ -1,20 +1,73 @@
 import data from "./data/rickandmorty/rickandmorty.js";
-//console.log(data);
+import { filterBySpecies } from "./data.js";
+
 const charactersImg = document.querySelector(".characters__img");
 const charactersDate = document.querySelector(".characters__date");
 const section1 = document.getElementById("section1");
+const dataCard = data.results.slice(0, 29);
 
-data.results.slice(0, 29).forEach((element) => {
-  const characterHTML = `
-  <button class="img" style="height: 200px; width: 200px;">
-    <img src="${element.image}" data-name="${element.name}" data-species="${element.species}" data-gender="${element.gender}" data-origin="${element.origin.name}" data-location="${element.location.name}" />
-    <div class="img--label">${element.name}</div>
-  </button>
-`;
-  charactersImg.innerHTML += characterHTML;
+// Función para mostrar los personajes
+function displayCharacters() {
+  charactersImg.innerHTML = "";
+  dataCard.forEach((element) => {
+    const characterHTML = `
+      <button class="img" style="height: 200px; width: 200px;">
+        <img src="${element.image}" data-name="${element.name}" data-species="${element.species}" data-gender="${element.gender}" data-origin="${element.origin.name}" data-location="${element.location.name}" data-episode="${element.episode}" />
+        <div class="img--label">${element.name}</div>
+      </button>
+    `;
+
+    charactersImg.innerHTML += characterHTML;
+  });
+}
+
+// Mostrar los personajes al cargar la página
+displayCharacters();
+
+//Crear el menú desplegable debajo de los personajes
+
+//filterForm.innerHTML = `
+//<label for="speciesSelect">Filtrar por especie:</label>
+// <select name="species" id="speciesSelect">
+// <option value="human">Human</option>
+//<option value="humanoid">Humanoid</option>
+//<option value="alien">Alien</option>
+//</select>
+//<button type="submit">Filtrar</button>
+//`;
+//charactersImg.insertAdjacentElement("afterend", filterForm);
+
+// Agregar el evento de submit al formulario
+const filterForm = document.getElementById("filterForm")
+
+filterForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const speciesSelect = document.getElementById("speciesSelect");
+  const selectedSpecies = speciesSelect.value;
+  filterBySpeciesAndDisplay(selectedSpecies);
+  console.log(selectedSpecies)
 });
-const episodesList = document.getElementById("episodes");
-episodesList.style.display = "none";
+
+// Función para filtrar los personajes por especie y mostrar los resultados
+function filterBySpeciesAndDisplay(species) {
+  const filteredData = filterBySpecies(dataCard, species);
+  displayFilteredData(filteredData);
+}
+
+// Función para mostrar los personajes filtrados
+function displayFilteredData(filteredData) {
+  charactersImg.innerHTML = "";
+  filteredData.forEach((element) => {
+    const characterHTML = `
+      <button class="img" style="height: 200px; width: 200px;">
+        <img src="${element.image}" data-name="${element.name}" data-species="${element.species}" data-gender="${element.gender}" data-origin="${element.origin.name}" data-location="${element.location.name}" data-episode="${element.episode}" />
+        <div class="img--label">${element.name}</div>
+      </button>
+    `;
+
+    charactersImg.innerHTML += characterHTML;
+  });
+}
 
 charactersImg.addEventListener("click", (event) => {
   if (event.target.tagName === "IMG") {
@@ -25,12 +78,13 @@ charactersImg.addEventListener("click", (event) => {
       gender: event.target.dataset.gender,
       origin: event.target.dataset.origin,
       location: event.target.dataset.location,
+      episode: event.target.dataset.episode,
     };
 
     showCharacterData(characterData);
     section1.style.display = "none";
   }
-});
+}); 
 
 function showCharacterData(data) {
   charactersDate.innerHTML = `
@@ -38,15 +92,10 @@ function showCharacterData(data) {
       <img src="${data.image}" />
       <p class="name">Nombre: ${data.name}</p>
       <p class="species">Especie: ${data.species}</p>
-      <p class="gender">Genero: ${data.gender}</p>
+      <p class="gender">Género: ${data.gender}</p>
       <p class="origin">Lugar de origen: ${data.origin}</p>
       <p class="location">Lugar donde vive: ${data.location}</p>
+      <p class="episode">Episodios: ${data.episode}</p>
     </div>
-    <select name="episodes" id="episodes">
-            <option value="episode_1">Capitulos</option>
-            <option value="episode_2">Capitulos 1</option>
-            <option value="episode_3">Capitulos 2</option>
-            <option value="episode_4">Capitulos 3</option>
-          </select>
   `;
 }
